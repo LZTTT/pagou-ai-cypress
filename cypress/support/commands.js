468525240f4f0b2ed4173e2ace953f63bebd7695
Tacio Zava <tacio.zava@hotmail.com>
2024-04-10 00:12:12 -0300

    feat: add commands login and registro

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,6 +2,18 @@ Cypress.Commands.add('RegistrarSubmit', () => {
     cy.get('.button').click()
 })
 
+Cypress.Commands.add('LoginSubmit', () => {
+    cy.get('.button').click()
+})
+
+Cypress.Commands.add('LoginEmail', ($emailLogin) => {
+    cy.get(':nth-child(1) > .container-input > .form-control').type($emailLogin)
+})
+
+Cypress.Commands.add('LoginSenha', ($senhaLogin) => {
+    cy.get(':nth-child(2) > .container-input > .form-control').type($senhaLogin)
+})
+
 Cypress.Commands.add('RegistroNome', ($nome) => {
     cy.get(':nth-child(1) > .container-input > .form-control').type($nome)
 })
@@ -16,4 +28,19 @@ Cypress.Commands.add('RegistroCelular', ($celular) => {
 
 Cypress.Commands.add('RegistroSenha', ($senha) => {
     cy.get(':nth-child(4) > .container-input > .form-control').type($senha)
+})
+
+Cypress.Commands.add('RegistroCompleto', ($rNomeCompleto, $rEmail, $rCelular, $rSenha) => {
+    cy.RegistroNome($rNomeCompleto)
+    cy.RegistroEmail($rEmail)
+    cy.RegistroCelular($rCelular)
+    cy.RegistroSenha($rSenha)
+    cy.RegistrarSubmit()
+})
+
+Cypress.Commands.add('Login', ($EmailLogin, $SenhaLogin) => {
+    cy.LoginEmail($EmailLogin)
+    cy.LoginSenha($SenhaLogin)
+
+    cy.LoginSubmit()
 })
