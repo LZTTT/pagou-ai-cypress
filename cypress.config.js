7ec6b779a5a2c35eabab3b33d389c6a4ed2e40d1
Tacio Zava <tacio.zava@hotmail.com>
2024-04-10 17:01:26 -0300

    feat: add commands para DB

--- cypress.config.js
+++ cypress.config.js
@@ -7,3 +7,29 @@ module.exports = defineConfig({
     },
   },
 });
+
+const pgp = require('pg-promise')();
+const { db } = require('./dbConfig');
+
+module.exports = {
+  e2e: {
+    baseUrl: 'http://localhost:3333',
+    setupNodeEvents(on, config) {
+      on('task', {
+        queryDatabase({ email }) {
+          const query = 'SELECT * FROM public.users';
+          return db.oneOrNone(query, email);
+        },
+      });
+
+      on('task', {
+        deleteEmailDatabase() {
+          const query = "DELETE FROM users WHERE email like 'pagouai@teste.com'";
+          return db.oneOrNone(query);
+        },
+      });
+
+    },
+    defaultCommandTimeout: 20000,
+  },
+};
